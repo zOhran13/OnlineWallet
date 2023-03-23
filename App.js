ac71d8fdf509ca832ee794e3d5d93fcfdc3a481e
zlata <zohran1@etf.unsa.ba>
2023-03-23 09:17:44 +0100

    Second Commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Novi pocetak!</Text>
+      <Text>Novi Sprint!</Text>
       <StatusBar style="auto" />
     </View>
   );
