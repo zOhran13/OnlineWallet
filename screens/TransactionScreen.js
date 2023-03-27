b9db10ae999be091bec18c34d330a67000a4ee5b
eminazolota <eminazolota3@gmail.com>
2023-03-27 09:33:12 +0200

    added submit method that uses POST request

--- screens/TransactionScreen.js
+++ screens/TransactionScreen.js
@@ -57,6 +57,44 @@ const TransactionScreen = () => {
     }
   };
 
+  const submitTransaction = async () => {
+    try {
+      checkTextInput();
+      let users=getUsers();
+      let flag=false;
+      users.forEach(element => {
+        if(element.name==textInputName && element.accountNumber==textInputNumber) flag=true;
+      });
+      if(flag) {
+        await fetch('url sa bekenda/transactions', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+
+          body: JSON.stringify({
+            name: textInputName,
+            accountNumber: textInputNumber,
+            currency: currency,
+            amount: textInputAmount,
+          }),
+        })
+          .then((response) => {
+            if(response.status==200) {
+              alert('Transaction successful!');
+              return;
+            } else {
+              alert('Transaction not successful!');
+              return;
+            }
+          });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
       <View style={styles.container}>
