c6d8ea984323333de73b4ecde11062e80e83982d
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-04-01 13:51:17 +0200

    first part of template list done

--- screens/TemplateListScreen.js
+++ screens/TemplateListScreen.js
@@ -10,8 +10,18 @@ const TemplateListScreen = ({ navigation }) => {
           style={styles.listTemplatesButton}
           onPress={() => navigation.navigate("Transaction")}
           //umjesto Transaction ide templejt koji zlata i naida rade
+          //request sa brojem templejta jednog korisnika treba biti urađen
         >
-          <Text style={styles.text}>Template</Text>
+          <Text style={styles.text}>Template 1</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.container2}>
+        <Pressable
+          style={styles.listTemplatesButton}
+          onPress={() => navigation.navigate("Transaction")}
+        >
+          <Text style={styles.text}>Template 2</Text>
         </Pressable>
       </View>
     </View>
@@ -25,16 +35,27 @@ const styles = StyleSheet.create({
     backgroundColor: "#1B1938",
     alignItems: "center",
     justifyContent: "center",
+    padding: 0,
+    marginTop: 0,
   },
   container1: {
     alignItems: "center",
     justifyContent: "center",
     borderColor: "black",
-    borderRadius: 40,
+    borderRadius: 10,
     backgroundColor: "#FFFF00",
-    padding: 50,
+    padding: 10,
     width: "80%",
-    marginTop: "80%",
+    marginBottom: 10,
+  },
+  container2: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "black",
+    borderRadius: 10,
+    backgroundColor: "#FFFF00",
+    padding: 10,
+    width: "80%",
   }
 });
 
